2cab511a9f7a31a9c480b3f669cac25900f9fb8d
Steven Ensslen <steven.ensslen@gmail.com>
2017-02-15 11:20:50 +1300

    Initalises with code from Decision One

---
+++ Company_Structure_SP.sql
@@ -0,0 +1,46 @@
+CREATE or Replace procedure COMPANY_STRUCTURE_SP as
+
+CURSOR Get_Roots is
+
+select CUSTOMER_KEY ROOT_KEY, decode(PARENT_KEY, NULL,'Y','N') HIGHEST_FLAG,
+ CUSTOMER_NAME ROOT_COMPANY
+from C
+where CURRENT_DESCRIPTION = 'Y';
+
+BEGIN
+
+execute immediate 'truncate table COMPANY_STRUCTURE';
+
+For Roots in Get_Roots 
+LOOP
+	insert into COMPANY_STRUCTURE 
+	(PARENT_KEY,
+ 	 SUBSIDIARY_KEY,
+ 	 COMPANY_LEVEL,
+ 	 SEQUENCE_NUMBER,
+ 	 LOWEST_FLAG,
+ 	 HIGHEST_FLAG,
+	 PARENT_COMPANY,
+ 	 SUBSIDIARY_COMPANY)
+	select
+	  Roots.ROOT_KEY,
+	  CUSTOMER_KEY,
+	  LEVEL,
+	  ROWNUM,
+	  'N',
+	  Roots.HIGHEST_FLAG,
+	  Roots.ROOT_COMPANY,
+	  CUSTOMER_NAME
+	from
+	  CUSTOMER
+	 Start with CUSTOMER_KEY = Roots.ROOT_KEY
+	 connect by prior CUSTOMER_KEY = PARENT_KEY;
+END LOOP;
+
+update COMPANY_STRUCTURE 
+   SET LOWEST_FLAG = 'Y'
+where not exists (select * from CUSTOMER
+where PARENT_KEY = COMPANY_STRUCTURE.SUBSIDIARY_KEY);
+
+COMMIT;
+END;  /* of procedure */
---
+++ Customer_HM_DDL.sql
@@ -0,0 +1,33 @@
+/* Define HV Customer Dimension with Type 2 SCD attribute CURRENT_DESCRIPTION (would also contain Start_Date and End_Date but not needed to prove CV HM solution) */
+
+Create table CUSTOMER (
+ CUSTOMER_KEY	     INTEGER NOT NULL,
+ CUSTOMER_NAME	     VARCHAR(50),
+ CUSTOMER_ID         CHAR(5),
+ PARENT_KEY	     INTEGER,
+ CURRENT_DESCRIPTION CHAR(1)
+);
+
+/* column order is Customer_key, Customer_name, Customer_ID, Parent_key, Current_Description */
+insert into customer values (100,'Pomegranate','C001',NULL,'N');
+insert into customer values (101,'iPip Design''C002',104,'Y');
+insert into customer values (102,'iSongs Store''C003',106,'Y');
+insert into customer values (103,'PicCzar Movies''C004',106,'N');
+insert into customer values (104,'POM Computing','C005',106,'Y');
+insert into customer values (105,'POM Store','C006',104,'Y');
+insert into customer values (106,'Pomegranate','C001',NULL,'Y');
+insert into customer values (107,'POMSoft','C007',104,'Y');
+insert into customer values (108,'POMStore.co.uk','C008',105,'Y');
+insert into customer values (109,'POMStore.com','C009',105,'Y');
+insert into customer values (110,'PicCzar Movies','C004',106,'Y');
+/* End example data */
+
+Create table COMPANY_STRUCTURE (
+ PARENT_KEY		INTEGER NOT NULL,
+ SUBSIDIARY_KEY		INTEGER NOT NULL,
+ COMPANY_LEVEL	        INTEGER NOT NULL,
+ SEQUENCE_NUMBER	INTEGER NOT NULL,
+ LOWEST_SUBSIDIARY	CHAR(1),
+ HIGHEST_PARENT		CHAR(1),
+ PARENT_COMPANY	 	VARCHAR(50),
+ SUBSIDIARY_COMPANY 	VARCHAR(50));
