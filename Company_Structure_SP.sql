b2a19260ad9d6756412b2356f074f8c8752ef21d
Steven Ensslen <steven.ensslen@gmail.com>
2017-02-15 12:46:35 +1300

    Formatting

--- Company_Structure_SP.sql
+++ Company_Structure_SP.sql
@@ -1,89 +1,56 @@
-CREATE or Replace procedure COMPANY_STRUCTURE_SP as
-
-
-
-CURSOR Get_Roots is
-
-
-
-select CUSTOMER_KEY ROOT_KEY, decode(PARENT_KEY, NULL,'Y','N') HIGHEST_FLAG,
-
- CUSTOMER_NAME ROOT_COMPANY
-
-from Customer
-where CURRENT_DESCRIPTION = 'Y';
-
-
+CREATE OR REPLACE PROCEDURE company_structure_sp AS
+    CURSOR get_roots IS
+        SELECT
+            customer_key root_key,
+            DECODE(parent_key,NULL,'Y','N') highest_flag,
+            customer_name root_company
+        FROM
+            customer
+        WHERE
+            current_description = 'Y';
 
 BEGIN
+    EXECUTE IMMEDIATE 'truncate table COMPANY_STRUCTURE';
+    FOR roots IN get_roots LOOP
+        INSERT INTO company_structure (
+            parent_key,
+            subsidiary_key,
+            company_level,
+            sequence_number,
+            lowest_subsidiary,
+            highest_parent,
+            parent_company,
+            subsidiary_company
+        ) SELECT
+            roots.root_key,
+            customer_key,
+            level,
+            ROWNUM,
+            'N',
+            roots.highest_flag,
+            roots.root_company,
+            customer_name
+        FROM
+            customer
+        START WITH
+            customer_key = roots.root_key
+        CONNECT BY
+            PRIOR customer_key = parent_key;
 
-execute immediate 'truncate table COMPANY_STRUCTURE';
+    END LOOP;
 
+    UPDATE company_structure
+        SET
+            lowest_subsidiary = 'Y'
+    WHERE NOT
+        EXISTS (
+            SELECT
+                *
+            FROM
+                customer
+            WHERE
+                parent_key = company_structure.subsidiary_key
+        );
 
-
-For Roots in Get_Roots 
-
-LOOP
-
-	insert into COMPANY_STRUCTURE 
-
-	(PARENT_KEY,
-
- 	 SUBSIDIARY_KEY,
-
- 	 COMPANY_LEVEL,
-
- 	 SEQUENCE_NUMBER,
-
- 	 LOWEST_SUBSIDIARY,
-
- 	 HIGHEST_PARENT,
-
-	 PARENT_COMPANY,
-
- 	 SUBSIDIARY_COMPANY)
-
-	select
-
-	  Roots.ROOT_KEY,
-
-	  CUSTOMER_KEY,
-
-	  LEVEL,
-
-	  ROWNUM,
-
-	  'N',
-
-	  Roots.HIGHEST_FLAG,
-
-	  Roots.ROOT_COMPANY,
-
-	  CUSTOMER_NAME
-
-	from
-
-	  CUSTOMER
-
-	 Start with CUSTOMER_KEY = Roots.ROOT_KEY
-
-	 connect by prior CUSTOMER_KEY = PARENT_KEY;
-
-END LOOP;
-
-
-
-update COMPANY_STRUCTURE 
-
-   SET LOWEST_SUBSIDIARY = 'Y'
-
-where not exists (select * from CUSTOMER
-
-where PARENT_KEY = COMPANY_STRUCTURE.SUBSIDIARY_KEY);
-
-
-
-COMMIT;
-
-END;  /* of procedure */
-
+    COMMIT;
+END company_structure_sp; 
